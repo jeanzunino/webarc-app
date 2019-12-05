dcc7523e5f3b3063a48a738dbf5410e012984159
jean.zunino <jean.zunino>
2019-12-04 22:33:22 -0300

    no message

--- undcon-backend/script/migration/public/V20191123231504580__tabela_menu_templates.sql
+++ undcon-backend/script/migration/public/V20191123231504580__tabela_menu_templates.sql
@@ -8,13 +8,12 @@ CREATE TABLE public.menu_template
 CREATE TABLE public.menu_template_item
 (
 	id SERIAL PRIMARY KEY,
-    menu_template_id SERIAL PRIMARY KEY,
+    menu_template_id int NOT NULL,
     tipo_recurso int NOT NULL,
     ordem int NOT NULL,
     CONSTRAINT fk_menu_template_item_menu_template_id FOREIGN KEY (menu_template_id)
         REFERENCES menu_template (id)
 );
 
-ALTER TABLE public.tenant ADD	column menu_template_id bigint NOT NULL;
+ALTER TABLE public.tenant ADD	column menu_template_id bigint;
 ALTER TABLE public.tenant ADD CONSTRAINT fk_tenant_menu_template_id FOREIGN KEY (menu_template_id) REFERENCES menu_template (id);
-

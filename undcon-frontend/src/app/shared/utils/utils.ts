a1d1b9508ec528994c83abc359b92e70ae7a7b35
jean.zunino <jean.zunino>
2020-05-03 01:25:03 -0300

    Método que tinha faltado comitar no utils openDialog

--- undcon-frontend/src/app/shared/utils/utils.ts
+++ undcon-frontend/src/app/shared/utils/utils.ts
@@ -1,8 +1,12 @@
 import { TranslateService } from '@ngx-translate/core';
+import { MDBModalService, MDBModalRef } from 'angular-bootstrap-md';
 
 import { SharedInjector } from "@shared/shared.module";
+import { Entity } from '@model/entity';
 
 const translate = SharedInjector.get(TranslateService);
+
+const modalService = SharedInjector.get(MDBModalService);
 
 export const getTranslate = (key, params = null) => {
   if (params) {
@@ -10,4 +14,20 @@ export const getTranslate = (key, params = null) => {
   } else {
     return translate.instant(key);
   }
+}
+
+export const openDialog = (item: Entity, obj: Object) => {
+  return modalService.show(obj, {
+    backdrop: true,
+    keyboard: true,
+    focus: true,
+    show: false,
+    ignoreBackdropClick: false,
+    class: 'modal-dialog-centered',
+    containerClass: '',
+    animated: true,
+    data: {
+      content: item
+    }
+  });
 }

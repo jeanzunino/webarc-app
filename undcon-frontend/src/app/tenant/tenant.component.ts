358c002fbb8589482dcfe1fc9b60d500b3300c76
Gabriel Bernardi <gabriel.s.bernardi@gmail.com>
2020-05-28 18:46:38 -0300

    Tenant

--- undcon-frontend/src/app/tenant/tenant.component.ts
+++ undcon-frontend/src/app/tenant/tenant.component.ts
@@ -1,21 +1,23 @@
-import { Component } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { MDBModalService } from "angular-bootstrap-md";
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MDBModalService } from 'angular-bootstrap-md';
 
-import { TenantService } from "@service/tenant/tenant.service";
-import { GridViewComponent } from "@component/grid-view/grid-view.component";
-import { Table } from "@shared/model/table";
-import { Tenant } from "@model/tenant";
+import { TenantService } from '@service/tenant/tenant.service';
+import { GridViewComponent } from '@component/grid-view/grid-view.component';
+import { Table } from '@shared/model/table';
+import { Tenant } from '@model/tenant';
+import { TenantEditComponent } from '@app/tenant/tenant-edit/tenant-edit.component';
 
 @Component({
-  selector: "app-tenant",
-  templateUrl: "./tenant.component.html",
+  selector: 'app-tenant',
+  templateUrl: './tenant.component.html',
 })
 export class TenantComponent extends GridViewComponent<Tenant> {
   tableValues = new Table()
-    .set("email", "tenant.email")
-    .set("phone", "tenant.phone")
-    .set("schemaName", "tenant.schemaName")
+    .set('name', 'tenant.name')
+    .set('email', 'tenant.email')
+    .set('schemaName', 'tenant.schemaName')
+    .set('phone', 'tenant.phone', '(00) 00000-0000')
     .get();
 
   constructor(
@@ -27,7 +29,7 @@ export class TenantComponent extends GridViewComponent<Tenant> {
   }
 
   onClickItem(item) {
-    //this.openDialog(item, TenantEditComponent);
+    this.openDialog(item, TenantEditComponent);
   }
 
   open() {
